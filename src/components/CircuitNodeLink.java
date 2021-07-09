7976fc4190a5831a1883ea5a99f71e67e3bfce46
a-bombarda <andrea.bombarda@unibg.it>
2021-07-09 10:46:37 +0200

    new version of circuit simulator

---
+++ src/components/CircuitNodeLink.java
@@ -0,0 +1,17 @@
+package components;
+public class CircuitNodeLink {
+	private int num;
+	private CircuitElm elm;
+	public CircuitElm getElm() {
+		return elm;
+	}
+	public void setElm(CircuitElm elm) {
+		this.elm = elm;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+}
